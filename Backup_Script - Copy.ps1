f7847960c27cb7101439558f003924f241bcf7c3
MattHughes7 <Matt_Hughes7@hotmail.com>
2015-02-22 01:01:53 -0500

    new

---
+++ Backup_Script - Copy.ps1
@@ -0,0 +1,4 @@
+Copy-Item -Path C:\Desktop -Destination d:\backups -recurse -Force
+Copy-Item -Path C:\Pictures -Destination d:\backups -recurse -Force
+Copy-Item -Path C:\Documents -Destination d:\backups -recurse -Force
+Copy-Item -Path C:\Dick -Destination d:\backups -recurse -Force
---
+++ Backup_Script.ps1
@@ -0,0 +1,3 @@
+Copy-Item -Path C:\Desktop -Destination d:\backups -recurse -Force
+Copy-Item -Path C:\Pictures -Destination d:\backups -recurse -Force
+Copy-Item -Path C:\Documents -Destination d:\backups -recurse -Force
